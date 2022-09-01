a8aeb6468fd80d1331557ba1c615c015ea5ee9cf
Wakwik <83170132+ekadharmaa@users.noreply.github.com>
2022-09-01 22:51:48 +0800

    Add files via upload

---
+++ semangat/js/script.js
@@ -0,0 +1,143 @@
+let nama, val;
+const url_string = document.URL;
+const url = new URL(url_string);
+let doi;
+
+if (url.searchParams.get('in') != null) {
+  doi = url.searchParams.get('in');
+} else {
+  doi = "Widya";
+}
+
+let footer = document.getElementById("credit");
+footer.innerHTML = doi;
+footer.href = "https://ekadharma.dev/";
+
+function time() {
+  var d = new Date();
+  var n = d.getHours();
+  console.log(n);
+  if (n >= 5 && n <= 10) {
+    return "Pagi"
+  } else if (n >= 10 && n <= 15) {
+    return "Siang"
+  } else if (n >= 15 && n <= 18) {
+    return "Sore"
+  } else if (n >= 18 && n <= 24) {
+    return "Malam"
+  }
+}
+
+function makan() {
+  switch (time()) {
+    case "Pagi":
+      return "sarapan"
+      break;
+    case "Siang":
+      return "makan siang"
+      break;
+    case "Malam":
+      return "makan malam"
+      break;
+      case "Sore":
+      return "makan"
+      break;
+    default:
+      break;
+  }
+}
+
+function selesai() {
+  const teks = document.getElementById('teks');
+  const btn = document.querySelector('.tombol');
+  teks.innerHTML = `Semangat ${doi} <i
+  class="fas fa-heart text-danger animate__animated animate__heartBeat animate__repeat-3"></i>`;
+  btn.classList.add('d-none');
+  teks.classList.remove('d-none');  
+}
+
+
+document.querySelector(".tombol").addEventListener('click', function () {
+  Swal.fire({
+    title: 'Hai widya!',
+    html: `Selamat ${time()}, Widya udah ${makan()}?`,
+    timer: 2500,
+    timerProgressBar: true,
+    showConfirmButton: false,
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      Swal.fire('Kalau belum jangan lupa makan yaa wid').then((result) => {
+        Swal.fire({
+          title: ' ',
+          html: `Aku tau kok widya lagi sibuk banget`,
+          timer: 2000,
+          timerProgressBar: true,
+          showConfirmButton: false,
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            Swal.fire({
+              title: ' ',
+              html: `Lagi pusing, cape banget`,
+              timer: 2000,
+              timerProgressBar: true,
+              showConfirmButton: false,
+            }).then((result) => {
+              if (result.dismiss === Swal.DismissReason.timer) {
+                Swal.fire('Semangat widya pkkmb nya :)').then((result) => {
+                  Swal.fire(
+                    'Jangan begadang!',
+                    '',
+                    'error'
+                  ).then((result) => {
+                    Swal.fire(
+                      'Jangan telat makan!',
+                      '',
+                      'error'
+                    ).then((result) => {
+                      Swal.fire('Aku dukung widya terus kok !').then((result) => {
+                        Swal.fire({
+                          title: 'Oh iya eka mau nanya',
+                          text: 'Ada sesuatu yang lagi widya pingin ga?',
+                          showDenyButton: true,
+                          confirmButtonText: `Ada ka`,
+                          denyButtonText: `Ga ada ka`,
+                        }).then((result) => {
+                          /* Read more about isConfirmed, isDenied below */
+                          if (result.isConfirmed) {
+                            Swal.fire({
+                              title: 'Mau apa?',
+                              input: 'text',
+                              inputPlaceholder: 'ex: Seblacc, boba, or etc',
+                              showCancelButton: false,
+                              inputValidator: (value) => {
+                                if (!value) {
+                                  return 'Isi dulu dong wid'
+                                }
+                              }
+                            }).then((result) => {
+                              Swal.fire('Okey wid', 'Nanti aku kabulin kemauan widya yaa', 'success').then((result) => {
+                                Swal.fire("Semangat ya wid! jangan menyerah^^").then((result) => {
+                                  selesai()
+                                });
+                              })
+                            })
+                          } else if (result.isDenied) {
+                            Swal.fire('Oh okedeh kalo ga ada').then((result) => {
+                              Swal.fire("Semangat ya wid! jangan menyerah^^").then((result) => {
+                                selesai()
+                              });
+                            })
+                          }
+                        })
+                      })
+                    })
+                  })
+                })
+              }
+            })
+          }
+        })
+      })
+    }
+  })
+});
