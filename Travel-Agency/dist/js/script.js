bf9adcef10227727afc047b62731ce6484b87270
Wakwik <83170132+ekadharmaa@users.noreply.github.com>
2023-03-13 20:41:36 +0800

    Add files via upload
    Travel

---
+++ Travel-Agency/dist/js/script.js
@@ -0,0 +1,15 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+  menuNav.classList.toggle("menu-active");
+});
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+  menuNav.classList.remove("menu-active");
+});
